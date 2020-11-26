ee77dd6fc56d87db4e55a8f9401fc800c1e39a7a
F5OEO <evariste@gmail.com>
2020-11-26 14:17:23 +0100

    remove sh as script is executable

--- board/pluto/overlay/www/rcv_analyse.php
+++ board/pluto/overlay/www/rcv_analyse.php
@@ -2,10 +2,10 @@
 
   if ( isset( $_GET[ 'cmd' ] ) ) {
   	if ( $_GET[ 'cmd' ] == 'start') {
-    	exec( 'sh /root/rcv_analyse.sh start' );
+    	exec( '/root/rcv_analyse.sh start' );
     	echo "Start";
     } else {
-    	exec( 'sh /root/rcv_analyse.sh stop' );
+    	exec( '/root/rcv_analyse.sh stop' );
     	echo "Stop";
     }
 
