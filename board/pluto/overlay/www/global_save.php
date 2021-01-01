9f123812d04be66df42f3a9b8834f367a75e06cc
f5uii <info@f5uii.net>
2021-01-02 00:06:25 +0100

    Save form transmit by array through POST (ajax)

--- board/pluto/overlay/www/global_save.php
+++ board/pluto/overlay/www/global_save.php
@@ -1,16 +1,14 @@
 <?php
-// F5UII : Saving global Adalm Pluto parameters to /opt/config.txt file
-var_dump($_POST);
+// F5UII : Saving global Adalm Pluto parameters to /opt/config.txt file in a ini format file
+//
+
+require ('lib/functions.php');
+
+//var_dump($_POST);
 
 if(isset($_POST)){
-	$string_tofile= "";
-	foreach($_POST as $key => $value){
-    $string_tofile.= $key . " " . $value . "\n";
+
+	write_php_ini($_POST,$_POST['file_dest'], $_POST['headlines']); //array with {data array, file, headines}
 }
 
-file_put_contents('config3.txt', $string_tofile);
-// 
-//exec ('route add default gw '.$_POST['gateway-eth0']);
-//copy('/www/settings-receiver.txt','/mnt/jffs2/settings-receiver.txt');
-}
 ?>
