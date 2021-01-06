cbd3eed675690d1db6ad8701d7eb232bde9ffa23
f5uii <info@f5uii.net>
2021-01-06 21:02:50 +0100

    added description in a new method serializeplus

--- board/pluto/overlay/www/lib/jquery.nestable.js
+++ board/pluto/overlay/www/lib/jquery.nestable.js
@@ -161,6 +161,31 @@
             return data;
         },
 
+        serializeplus: function()
+        {
+            var data,
+                depth = 0,
+                list  = this;
+                step  = function(level, depth)
+                {
+                    var array = [ ],
+                        items = level.children(list.options.itemNodeName);
+                        
+                    items.each(function()
+                    {   desc =  $(' ol [data-id="'+$(this).data()['id']+'"]').find('div').html() ;// level.children(list.options.itemNodeName).children('.'+list.options.handleClass);
+                        var li   = $(this),
+                            item = '{"desc":"'+desc+'", "id":"'+li.data()['id']+'"}',
+                            sub  = li.children(list.options.listNodeName);
+                        if (sub.length) {
+                            item.children = step(sub, depth + 1);
+                        }
+                        array.push(item);
+                    });
+                    return array;
+                };
+            data = step(list.el.find(list.options.listNodeName).first(), depth);
+            return data;
+        },
         serialise: function()
         {
             return this.serialize();
