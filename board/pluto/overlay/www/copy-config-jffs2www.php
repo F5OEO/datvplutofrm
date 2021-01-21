2c68adcde1268d48bacd4ac00f46b11d658b40cc
F5OEO <evariste@gmail.com>
2021-01-21 19:03:08 +0100

    mispell

--- board/pluto/overlay/www/copy-config-jffs2www.php
+++ board/pluto/overlay/www/copy-config-jffs2www.php
@@ -2,7 +2,7 @@
 // when patch is not included in firmware, the config file is saved /mnt/jffs2/
 // restore a config file from /mnt/jffs2/ to /www/
 
-if (!file_exists('/www/jffs2/etc/settings-receiver.txt')){
+if (!file_exists('/mnt/jffs2/etc/settings-receiver.txt')){
     copy('/mnt/jffs2/etc/settings-receiver.txt', '/www/settings-receiver.txt');
 }
 
