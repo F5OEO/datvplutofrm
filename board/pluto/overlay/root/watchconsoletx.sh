197e6478ecaccf7f07949e3641716ddd5a3583d6
F5OEO <evariste@gmail.com>
2021-01-23 15:28:59 +0100

    OPtimized watchconsole with inotifywait

--- board/pluto/overlay/root/watchconsoletx.sh
+++ board/pluto/overlay/root/watchconsoletx.sh
@@ -20,37 +20,14 @@ echo manual_tx_quad > /sys/bus/iio/devices/iio:device1/calib_mode
 echo 0x26 0x10 > /sys/kernel/debug/iio/iio:device1/direct_reg_access
 pttoff
 
-#Set -40db forcing Rx Mode
-#echo -40 > /sys/bus/iio/devices/iio:device1/out_voltage0_hardwaregain
 
 while :
 do
-#inotifywait -m -e modify /sys/bus/iio/devices/iio\:device1/out_voltage0_hardwaregain
-
+inotifywait -e modify /sys/bus/iio/devices/iio\:device1/out_voltage0_hardwaregain
 gain=$(cat /sys/bus/iio/devices/iio:device1/out_voltage0_hardwaregain)
-powerdown=$(cat /sys/bus/iio/devices/iio:device1/out_altvoltage1_TX_LO_powerdown)
-
 if [ "$gain" = "-40.000000 dB" ] ; then
-modeconsole=1
-echo 0 > /sys/bus/iio/devices/iio:device1/out_altvoltage1_TX_LO_powerdown
-echo manual_tx_quad > /sys/bus/iio/devices/iio:device1/calib_mode
-fi
-
-if [ "$powerdown" = "1" ] ; then
-modeconsole=0
-fi
-
-
-if [ "$modeconsole" = "1" ] ; then
-	if [ "$gain" = "-40.000000 dB" ] ; then
-        	echo RX $gain $powerdown
-		pttoff
-	else
-		echo TX $gain $powerdown
-		ptton
-	fi
+pttoff
 else
-      #echo mode dvb
+ptton
 fi
-sleep 0.1
 done
