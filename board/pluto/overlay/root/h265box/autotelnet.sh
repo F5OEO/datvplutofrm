6bf71df9fd54ed3b81db127ad2eb426d5311ebce
F5OEO <evariste@gmail.com>
2021-01-30 17:11:51 +0100

    Add sleep for telnet session

--- board/pluto/overlay/root/h265box/autotelnet.sh
+++ board/pluto/overlay/root/h265box/autotelnet.sh
@@ -3,6 +3,14 @@ sleep 1
 while read ligne
 do
 echo  $ligne
+
+if echo "$ligne" | grep -q "mount"; then
+  sleep 2;
+fi
+
+if echo "$ligne" | grep -q "reboot"; then
+  sleep 60;
+fi
 sleep 0.1
 done < patch_for_telnet.txt) | nc 192.168.1.120 23
 
