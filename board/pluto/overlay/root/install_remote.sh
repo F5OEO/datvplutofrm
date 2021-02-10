73faa57ed3274d656fbaeffbbc2e5c10fbaf88f1
F5OEO <evariste@gmail.com>
2021-02-10 16:24:18 +0100

    install remote assistance*

---
+++ board/pluto/overlay/root/install_remote.sh
@@ -0,0 +1,5 @@
+wget -P /root/ https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip /root/ngrok-stable-linux-arm.zip -d /root
+root/ngrok authtoken 7g11Eh4DHP44u6siYpBQW_69kWZEKBKyydBHf9PTzuS
+cp /root/ngrok.yml /root/.ngrok2/ngrok.yml
+/root/ngrok start --all
