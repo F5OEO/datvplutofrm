df94e3dde9e7bc3cdc491c0f0d94214c5d2f6bf4
F5OEO <evariste@gmail.com>
2021-03-13 18:15:17 +0100

    Add workaround for SDRCOnsole PTTON at startup : when gain max (0db) wait 0.5 second to be sure it is not the init

--- board/pluto/overlay/root/watchconsoletx.sh
+++ board/pluto/overlay/root/watchconsoletx.sh
@@ -20,14 +20,24 @@ echo manual_tx_quad > /sys/bus/iio/devices/iio:device1/calib_mode
 echo 0x26 0x10 > /sys/kernel/debug/iio/iio:device1/direct_reg_access
 pttoff
 
-
 while :
 do
 inotifywait -e modify /sys/bus/iio/devices/iio\:device1/out_voltage0_hardwaregain
 gain=$(cat /sys/bus/iio/devices/iio:device1/out_voltage0_hardwaregain)
 if [ "$gain" = "-40.000000 dB" ] ; then
-pttoff
+echo "SdrConsole PTT OFF"
 else
-ptton
+        if [ "$gain" = "0.000000 dB" ] ; then
+                sleep 0.5
+                gain=$(cat /sys/bus/iio/devices/iio:device1/out_voltage0_hardwaregain)
+                if [ "$gain" = "0.000000 dB" ] ; then
+                        echo "SdrConsole Power Max PTT ON"
+                        ptton
+                fi
+        else
+                echo "SdrConsole PTT ON"
+                ptton
+        fi
 fi
 done
+
